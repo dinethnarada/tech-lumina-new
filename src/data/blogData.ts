4dfc26a238c8bbd7506760de59784c5187600c20
yasirurathnayaka <yasirurathnayaka97@gmail.com>
2025-05-02 22:17:54 +0530

    fis blog image not showing issue

--- src/data/blogData.ts
+++ src/data/blogData.ts
@@ -4,7 +4,7 @@ export const blogPosts: BlogPost[] = [
   {
     title: "CMS vs Custom Coding : What's the Best Fit for Your Website?",
     content: "Explore the pros and cons of using a CMS versus custom coding for your website. Learn which approach best suits your business needs, considering factors like scalability, security, and maintenance.",
-    imageSrc: "/blog/cms-vs-custom.jpg",
+    imageSrc: "images/blog/cms-vs-custom.jpg",
     slug: "cms-vs-custom-coding",
     date: "April 15, 2025",
     readTime: "7 min read"
@@ -12,7 +12,7 @@ export const blogPosts: BlogPost[] = [
   {
     title: 'Service Level Agreement (SLA) : Explained with a Real World Example',
     content: "Let's say a client approaches us to build a website for their bakery that sells products across the entire country. This isn't just any small website. It's a large-scale platform that will handle lots of traffic and transactions. For projects like this, it's important to clearly define expectations between the client and the development team. This is where a Service Level Agreement (SLA) comes in.",
-    imageSrc: '/blog/sla.jpg',
+    imageSrc: 'images/blog/sla.jpg',
     slug: 'service-level-agreement',
     date: 'April 18, 2025',
     readTime: '5 min read'
